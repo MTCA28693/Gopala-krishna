b5d638b747bce25df15996b06a9d235fe7f73e37
MTCA28693 <126249692+MTCA28693@users.noreply.github.com>
2023-02-25 14:41:18 +0530

    Add files via upload

---
+++ Program304d - Copy.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Program304d
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		a=(a*b)/(b=a);
+			System.out.println("numbers after swapping"+a+" "+b);
+	}
+}
+		
+		
+
